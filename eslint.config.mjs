9ca99fed5da5e4ee4d890c0fd3985a200fed1a9d
Nico Welles <nico@welles.email>
2025-11-16 14:53:30 +0100

    Install typescript-eslint in project

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+﻿import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  prettierConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    ignores: ["dist", "node_modules", "eslint.config.mjs"],
+  },
+);
